7ceb0e5de7206d9803d210b983e23706ecf19c86
Darrik Mazey <darrik@darmasoft.com>
2010-01-03 16:16:36 -0500

    room list msg

--- erl_camp-1.0/include/campfire_srv.hrl
+++ erl_camp-1.0/include/campfire_srv.hrl
@@ -1,3 +1,7 @@
 
 %% campfire auth die request
 -record(msg_campfire_srv_die, { sender, why }).
+
+%% campfire room list request
+-record(msg_campfire_srv_list_rooms, { sender }).
+
--- erl_camp-1.0/src/campfire_srv.erl
+++ erl_camp-1.0/src/campfire_srv.erl
@@ -5,6 +5,8 @@
 -export([start/0]).
 -export([stop/0]).
 
+-export([list_rooms/0]).
+
 -export([init/0]).
 
 -include("include/campfire_srv.hrl").
@@ -31,8 +33,16 @@ loop(State) ->
 		#msg_campfire_srv_die{} ->
 			debug:log("campfire_srv: shutting down"),
 			true;
+		#msg_campfire_srv_list_rooms{sender=Sender} ->
+			debug:log("campfire_srv: received list_rooms from ~p", [Sender]),
+			CA = campfire_auth:find(Sender),
+			debug:log("campfire_srv: found ~p", [CA]),
+			loop(State);
 		M ->
 			debug:log("campfire_srv: received unknown message: ~p", [M]),
 			loop(State)
 	end.
 
+list_rooms() ->
+	campfire_srv ! #msg_campfire_srv_list_rooms{sender=self()},
+	true.
